e48cb4ac0b86994012d5b7d0bcd90ae9e26cd4ee
Mario Suharja <mario@mss.io>
2026-01-28 20:41:17 -0700

    Format Code

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,12 +3,10 @@ import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
 export default authkitMiddleware({
   middlewareAuth: {
     enabled: true,
-    unauthenticatedPaths: [
-      "/",
-    ],
+    unauthenticatedPaths: ["/"],
   },
 });
 
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ['/', '/generate', '/api/generate/:type'] };
+export const config = { matcher: ["/", "/generate", "/api/generate/:type"] };
